7858034249844abe1f1bfb669db3d5aff541599e
JK2018 <khalifa_jonathan@hotmail.com>
2018-11-29 21:44:17 +0100

    twig

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HomeController extends Controller{
+    
+    /**
+     * @Route("/", name="homepage")
+     * @return void
+     */
+    public function home(){
+        //...
+        return $this->render('home.html.twig');
+    }
+}
+
+
+
+
+
+
+?>
