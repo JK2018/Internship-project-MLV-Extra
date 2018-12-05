6f19257adc47e510ce03a8311a70a819183e2046
JK2018 <khalifa_jonathan@hotmail.com>
2018-12-05 17:31:20 +0100

    added show twig

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+        $ads = $repo->findAll();
+
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+           // 'controller_name' => 'AdController',
+           'ads' => $ads
+        ]);
+    }
+
+    /**
+     * shows one ad.
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        // recovers the ad that corresponds to $slug.
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
         ]);
     }
 }
